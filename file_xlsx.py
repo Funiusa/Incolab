95d10faab54da6f61e53f13761ecf04d091a10bb
wmadison <wmadison@student.21-school.ru>
2022-11-21 12:43:16 +0500

    initial commit

---
+++ file_xlsx.py
@@ -0,0 +1,134 @@
+import openpyxl
+from openpyxl import Workbook
+from openpyxl.styles import Alignment
+from openpyxl.comments import Comment
+from openpyxl.styles import PatternFill
+
+
+class FileXlsx:
+    """ Class for creating new instance of xlsx with parameters """
+
+    def __init__(self, path_to_file, file_name, create_empty=False):
+        self._path_to_file = path_to_file + file_name
+        self._file_name = file_name
+        self._workbook = Workbook()
+        if create_empty:
+            self._workbook.save(path_to_file)
+
+    def save(self):
+        """ Save all work """
+        self._workbook.save(self._path_to_file)
+
+    def close(self):
+        """ Close file """
+        self._workbook.close()
+
+    def one_cell_filling(self, row, column, value):
+        """ Add element in one cell """
+        sheet = self._workbook.active
+        try:
+            sheet.cell(row, column).value = value
+        except ValueError:
+            print(f"\nERROR: Cannot convert {value} to Excel. "
+                  "Use row/column fillings methods instead.")
+        except Exception as e:
+            print(f"\nERROR: {e}")
+
+    def wrap_cell(self, row, column):
+        """ Wrap cell """
+        sheet = self._workbook.active
+        try:
+            sheet.cell(row,column).alignment = Alignment(wrapText=True, vertical='top', horizontal='center')
+        except Exception as e:
+            print(f"\nERROR: {e}")
+
+    def wrap_row(self, row, start_column, end_column):
+        """ Wrap cells in rows. """
+        try:
+            for column in range(start_column, end_column):
+                self.wrap_cell(row, column)
+        except Exception as e:
+            print(f"ERROR: {e}")
+
+    def wrap_column(self, column, start_row, end_row):
+        """ Wrap cells in columns """
+        try:
+            for row in range(start_row, end_row):
+                self.wrap_cell(row, column)
+        except Exception as e:
+            print(f"ERROR: {e}")
+
+    def row_filling(self, row, column_end, values):
+        """
+            Filling cells with values in row.
+            All columns and rows must be start at least 1
+        """
+        try:
+            for column in range(column_end):
+                self.one_cell_filling(row, column + 1, values[column])
+        except Exception as e:
+            print(f"ERROR: {e}")
+
+    def column_filling(self, column, start_row, values):
+        """
+            Filling cells with values in column
+            All columns and rows must be start at least 1
+        """
+        try:
+            for value in values:
+                self.one_cell_filling(start_row, column, value)
+                start_row += 1
+        except Exception as e:
+            print(f"ERROR: {e}")
+
+    # Create subclass for comments
+    def comment_one(self, row, column, comment, authon='Noname'):
+        """
+            Add comments on the cell
+        """
+        try:
+            sheet = self._workbook.active
+            sheet.cell(row, column).comment = Comment(comment, authon)
+        except Exception as e:
+            print(f"ERROR: {e}")
+
+    def comment_row(self, row, first_column, end_column, comment, author='Noname'):
+        """Add comment for cells in row"""
+        try:
+            for i in range(end_column):
+                self.comment_one(row, first_column + i, comment, author)
+        except Exception as e:
+            print(f"ERROR: {e}")
+
+    def comment_column(self, column, start_row, end_row, comment, author):
+        """Add comment for cells in column"""
+        try:
+            for i in range(end_row - 1):
+                self.comment_one(start_row + i, column, comment, author)
+        except Exception as e:
+            print(f"ERROR: {e}")
+
+    # Create subclass for colors
+    def color_one_cell(self, row, column, color, patrn_type='solid'):
+        """Add background color for one cell"""
+        try:
+            sheet = self._workbook.active
+            sheet.cell(row, column).fill = PatternFill(patternType=patrn_type, fgColor=color)
+        except Exception as e:
+            print(f"ERROR: {e}")
+
+    def color_row(self, row, first_column, end_column, color, patrn_type='solid'):
+        """Add background color for cells in row"""
+        try:
+            for i in range(end_column):
+                self.color_one_cell(row, first_column + i, color, patrn_type)
+        except Exception as e:
+            print(f"ERROR: {e}")
+
+    def color_column(self, column, start_row, end_row, color, patrn_type='solid'):
+        """Add background color for cells in column"""
+        try:
+            for i in range(end_row - 1):
+                self.color_one_cell(start_row + i, column, color, patrn_type)
+        except Exception as e:
+            print(f"ERROR: {e}")
---
+++ parsXLSX.py
@@ -0,0 +1,128 @@
+import openpyxl
+import pandas as pd
+from pathlib import Path
+from math import isnan
+from openpyxl.styles import PatternFill
+import time
+from openpyxl import Workbook
+from openpyxl.styles import Alignment
+
+from file_xlsx import FileXlsx
+
+xlsx_file = Path('./Silvery_Port.xlsx')
+path_for_new_file = './Povogonka.xlsx'
+
+COLOR_HEADER = 'FFC000'
+COLOR_GRAY = 'BDBBB6'
+COLOR_PINK = 'E5D1D0'
+LIGHT_GREEN = 'F0F6E8'
+
+# Retrieve cell value
+listCabin = [57588279, 64130933, 56918501, 56249444, 61893335]
+
+
+def fill_row(sheet, row, col, color):
+    for i in range(1, col):
+        sheet.cell(row, i).fill = PatternFill(patternType='solid', fgColor=color)
+
+
+def create_xlsx_file_header(path, filename, names_list):
+    count = len(names_list)
+    newxl = FileXlsx(path, filename)
+    newxl.row_filling(1, count, names_list)
+    newxl.color_row(1, 1, count, COLOR_HEADER)
+    newxl.wrap_row(1, 1, count + 1)
+    return newxl
+
+
+def get_values(sheet, rw, colons):
+    return [sheet.cell(row=rw, column=colon).value for colon in range(1, colons + 1)]
+
+
+def fill_rows(sheet, row):
+    column = 1
+    while sheet.cell(row, column).value:
+        sheet.cell(row, column).fill = PatternFill(patternType='solid', fgColor=COLOR_PINK)
+        column += 1
+    """Add date on the last cell and fill background"""
+    sheet.cell(row, column).value = time.strftime("%x")
+    sheet.cell(row, column).fill = PatternFill(patternType='solid', fgColor=COLOR_GRAY)
+
+
+def add_new_elements():
+    pass
+
+
+def get_weight_sum(path_to_file):
+    # Read the values of the file in the dataframe
+    excel_data = pd.read_excel(path_to_file)
+    # Convert file in dict for keys and values
+    exel_values = excel_data.to_dict('dict')
+    cabin_num = []
+    weight = excel_data.get("Фактический вес")
+
+    for name, val in exel_values.items():
+        if name == 'Номер вагона':
+            for key, cabin in val.items():
+                if cabin in listCabin:
+                    cabin_num.append(key)
+
+    return [weight[elem] for elem in cabin_num]
+
+
+def get_dataFromMainXSLX(path_to_file):
+    main_workbook = openpyxl.load_workbook(path_to_file)  # path to the Excel file
+    sheet = main_workbook.active
+    rows = sheet.max_row
+    columns = sheet.max_column
+    header_names = [cell.value for cell in list(sheet.rows)[0] if cell.value]
+    header_names.append('Дата послупления')
+
+    """ Checking the data """
+    weight_sum = sum(get_weight_sum(path_to_file))
+    print(weight_sum)
+
+    """
+        Get the values from the main table
+        First row and column is header
+    """
+    values_list = []
+    for row in range(2, rows):
+        for column in range(1, columns):
+            cell = sheet.cell(row, column)
+            if cell.value in listCabin:
+                values_list += [get_values(sheet, row, column + 1) + [time.strftime("%x")]]
+                fill_rows(sheet, row)
+
+    """ Safe changes in the main file """
+    last_column = len(values_list[0])
+    count_rows = len(values_list)
+    main_workbook.save(path_to_file)
+
+    newxls = create_xlsx_file_header('./', 'test.xlsx', header_names)
+    row = 2  # Because we already have a header
+    for value in values_list:
+        newxls.row_filling(row, len(value), value)
+        row += 1
+    print(values_list)
+
+    newxls.one_cell_filling(last_column, last_column + 1, "Сумма:")
+    newxls.color_one_cell(last_column, last_column + 1, COLOR_HEADER)
+    newxls.one_cell_filling(last_column, last_column + 2, weight_sum)
+
+    newxls.one_cell_filling(last_column + 1, last_column + 1, "Кол-во:")
+    newxls.color_one_cell(last_column + 1, last_column + 1, COLOR_HEADER)
+    newxls.one_cell_filling(last_column + 1, last_column + 2, count_rows)
+
+
+    # Do 
+    """ Sum of all weight """
+    newxls.color_row(last_column, 1, last_column, LIGHT_GREEN)
+    newxls.one_cell_filling(last_column + 1, last_column - 1, "Итого:")
+    newxls.color_row(last_column + 1, 1, last_column, COLOR_HEADER)
+    newxls.save()
+
+
+if __name__ == "__main__":
+    # take_values()
+    get_dataFromMainXSLX(xlsx_file)
